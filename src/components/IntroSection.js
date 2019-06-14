fb4d8a0cdb39a4b33d16b7f90c8b09653955dcdf
Anden Acitelli <a.acitelli@gmail.com>
2019-06-13 21:39:18 -0400

    Basic file structure additions

---
+++ src/components/IntroSection.js
@@ -0,0 +1,8 @@
+// NPM Imports
+import React from "react";
+
+// Component Imports
+
+// Style Imports
+import "../css/Universal.css";
+import "../css/IntroSectionStyles.css";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+// NPM Imports
 import React from "react"
+
+// Component Imports 
+import IntroSection from "../components/IntroSection";
+import SkillsSection from "../components/SkillsSection";
+import ProjectsSection from "../components/ProjectsSection";
+import ContactSection from "../components/ContactSection";
+
+// Style Imports
 
 export default () => <div>Hello world!</div>

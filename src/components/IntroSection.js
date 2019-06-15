1ad229cb063c36c1c4b6b9e131af8f9532c4e07c
Anden Acitelli <a.acitelli@gmail.com>
2019-06-15 14:13:24 -0400

    Large styling changes, misc. other changes

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -11,10 +11,12 @@ export default function IntroSection()
 {
     return (
         <div id="IntroSection">
-            <div>Anden Acitelli</div>
-            <p>Hey! I'm a Full-Stack Web Developer and Software Developer based out of Pittsburgh, PA.</p>
-            <p>I build websites with ReactJS and am comfortable in Java, C/C++, and Python.</p>
-            <p>Check out my <a href="/blog">blog</a> or my <a href="https://drive.google.com/open?id=1nlJzHsSkNTlFM0Xu_uAItzb8M-6_duzt">Résumé</a> for more about me! You can get into contact by emailing hello@andenacitelli.com</p> and I'll get back to you as soon as possible. 
+            <div>
+                <h1>Anden Acitelli</h1>
+                <p>Hey! I'm a Full-Stack Web Developer and Software Developer based out of Pittsburgh, PA.</p>
+                <p>I build websites with ReactJS and am comfortable in Java, C/C++, and Python.</p>
+                <p>Check out my <a href="/blog">blog</a> or my <a href="https://drive.google.com/open?id=1nlJzHsSkNTlFM0Xu_uAItzb8M-6_duzt">Résumé</a> for more about me! You can get into contact by emailing hello@andenacitelli.com and I'll get back to you as soon as possible. </p>
+            </div>            
         </div>
     )    
 }

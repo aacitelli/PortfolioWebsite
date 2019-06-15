58a83cfdaf68d48f815b543fef0e83b5eef9e99b
Anden Acitelli <a.acitelli@gmail.com>
2019-06-15 13:35:42 -0400

    Built basic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,13 @@ export default function Navbar()
 {
     return (
         <nav id="navbar">
-
+            <ul>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
         </nav>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@
 import React from "react"
 
 // Component Imports 
+import Navbar from "../components/Navbar";
 import IntroSection from "../components/IntroSection";
 import SkillsSection from "../components/SkillsSection";
 import ProjectsSection from "../components/ProjectsSection";
 import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
 
 // Style Imports
 
@@ -13,10 +15,12 @@ export default () =>
 {
     return (
         <div>
+            <Navbar />
             <IntroSection />
             <SkillsSection />
             <ProjectsSection />
             <ContactSection />
+            <Footer />
         </div>   
     )
      

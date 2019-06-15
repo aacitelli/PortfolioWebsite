6efdfc2a301a8c048d99157d35dd2702eef61912
Anden Acitelli <a.acitelli@gmail.com>
2019-06-15 14:13:10 -0400

    Added navbar/footer and implemented meta tags w/ React Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 // NPM Imports
-import React from "react"
+import React from "react";
+import { Helmet } from "react-helmet";
 
 // Component Imports 
 import Navbar from "../components/Navbar";
@@ -14,7 +15,12 @@ import Footer from "../components/Footer";
 export default () => 
 {
     return (
-        <div>
+        <div id="siteContainer">
+            <Helmet>
+                <meta charSet="utf-8"></meta>
+                <title>Anden Acitelli - Full-Stack Web and Software Developer</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            </Helmet>
             <Navbar />
             <IntroSection />
             <SkillsSection />

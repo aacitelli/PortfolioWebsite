aaa071ff713f3c04e85d5e21bdfdbbd407b65297
Anden Acitelli <a.acitelli@gmail.com>
2019-06-16 12:25:01 -0400

    Switched to basic horizontal navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,11 +24,14 @@ export default () =>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
             </Helmet>
             <Navbar />
-            <IntroSection />
-            <SkillsSection />
-            <ProjectsSection />
-            <ContactSection />
-            <Footer />
+            <div id="rightContent">
+                <IntroSection />
+                <SkillsSection />
+                <ProjectsSection />
+                <ContactSection />
+                <Footer />
+            </div>
+            
         </div>   
     )
      

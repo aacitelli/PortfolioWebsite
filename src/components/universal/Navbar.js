3feafc1633d8860bcd7a397acbd052b4183cba46
Anden Acitelli <a.acitelli@gmail.com>
2019-06-16 16:19:37 -0400

    Added basic picture links at bottom of navbar

--- src/components/universal/Navbar.js
+++ src/components/universal/Navbar.js
@@ -8,23 +8,53 @@ import { Link } from "gatsby";
 import "../../css/universal/UniversalStyles.css";
 import "../../css/universal/NavbarStyles.css";
 
+// Image Imports
+import resume from "../../../static/materialicons/resume.svg";
+import github from "../../../static/materialicons/github.svg";
+
 export default function Navbar() 
 {
     return (
         <nav id="navbar">
             <ul>
-                { /* TODO: MAKE THE ENTIRE SECTION CLICKABLE AND NOT JUST THE TEXT */ }
-                <li className="navbarLink1"><Link to="/">About</Link></li>
-                <li><div className="navbarSeparator"></div></li>
-                <li className="navbarLink2"><Link to="/skills">Skills</Link></li>
-                <li><div className="navbarSeparator"></div></li>
-                <li className="navbarLink3"><Link to="/projects">Projects</Link></li>
-                <li><div className="navbarSeparator"></div></li>
-                <li className="navbarLink4"><Link to="/blog">Blog</Link></li>
-                <li><div className="navbarSeparator"></div></li>
-                <li className="navbarLink5"><Link to="/contact">Contact</Link></li>
-                <li><div className="navbarSeparator"></div></li>
+
+                <li id="navbarLink1" className="lighterNavbarButton">
+                    <div class="fullSizeOfParent">
+                        <Link to="/">About</Link>
+                    </div>
+                </li>
+
+                <li id="navbarLink2" className="darkerNavbarButton">
+                    <div className="fullSizeOfParent">
+                        <Link to="/skills">Skills</Link>
+                    </div>
+                </li>
+
+                <li id="navbarLink3" className="lighterNavbarButton">
+                    <div className="fullSizeOfParent">
+                        <Link to="/projects">Projects</Link>
+                    </div>                    
+                </li>
+
+                <li id="navbarLink4" className="darkerNavbarButton">
+                    <div className="fullSizeOfParent">
+                        <Link to="/blog">Blog</Link>
+                    </div>                    
+                </li>
+
+                <li id="navbarLink5" className="lighterNavbarButton">
+                    <div className="fullSizeOfParent">
+                        <Link to="/contact">Contact</Link>
+                    </div>
+                </li>
+
             </ul>
+
+            <div>
+                <img alt="Resume Logo" width="35px" height="35px" src={resume}></img>
+                <img alt="GitHub Logo" width="35px" height="35px" src={github}></img>
+            </div>
+
         </nav>
     )
 }

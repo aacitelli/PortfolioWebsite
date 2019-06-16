0c3c01522c329f9ecd7ce85ee99263892eb9f435
Anden Acitelli <a.acitelli@gmail.com>
2019-06-16 15:21:25 -0400

    Lots of styling changes to navbar

--- src/components/universal/Navbar.js
+++ src/components/universal/Navbar.js
@@ -13,11 +13,16 @@ export default function Navbar()
     return (
         <nav id="navbar">
             <ul>
-                <li><Link to="/">About</Link></li>
-                <li><Link to="/skills">Skills</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li className="navbarLink1"><Link to="/">About</Link></li>
+                <li><div className="navbarSeparator"></div></li>
+                <li className="navbarLink2"><Link to="/skills">Skills</Link></li>
+                <li><div className="navbarSeparator"></div></li>
+                <li className="navbarLink3"><Link to="/projects">Projects</Link></li>
+                <li><div className="navbarSeparator"></div></li>
+                <li className="navbarLink4"><Link to="/blog">Blog</Link></li>
+                <li><div className="navbarSeparator"></div></li>
+                <li className="navbarLink5"><Link to="/contact">Contact</Link></li>
+                <li><div className="navbarSeparator"></div></li>
             </ul>
         </nav>
     )

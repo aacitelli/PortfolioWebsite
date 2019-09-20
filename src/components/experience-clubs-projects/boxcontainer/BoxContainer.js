f36d8dadc2cd140f3b7e7730f9d9793f80430f9d
Anden Acitelli <a.acitelli@gmail.com>
2019-09-19 23:24:44 -0400

    Implemented position; Restructured box container to look better

--- src/components/experience-clubs-projects/boxcontainer/BoxContainer.js
+++ src/components/experience-clubs-projects/boxcontainer/BoxContainer.js
@@ -7,9 +7,10 @@ const BoxContainer = (props) =>
     return (
         <div className="boxContainer">
             <h1>{props.title}</h1>
-            <p><b>{props.languages}</b></p>
+            <p className="boxContainerPosition">{props.position}</p>
             <p>{props.description1}</p>
             <p>{props.description2}</p>
+            <p className="boxContainerLanguages">{props.languages}</p>
         </div>
     )
 }
